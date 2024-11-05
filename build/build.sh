9a9cbae3095390d19c3799a26307914c01f9e4be
EgorNiKO <niko_egor@mail.ru>
2024-11-05 19:50:43 +0000

    Add lib libspdlog-dev

--- build/build.sh
+++ build/build.sh
@@ -166,7 +166,7 @@ pushd wfb-ng
 popd
 
 # PixelPilot_rk / fpvue
-apt install -y libcairo-dev libdrm-dev 
+apt install -y libcairo-dev libdrm-dev libspdlog-dev
 apt --no-install-recommends -y install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools 
 
 case $BOARD in
